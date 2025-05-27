db7bb2e55418be057ef9d01c0c288450a85e72c5
gfaizank <faizan2017.fk@gmail.com>
2025-05-27 19:34:19 +0530

    Added api

--- ui/src/components/Contact.jsx
+++ ui/src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     setStatus({ submitting: true, submitted: false, success: false, message: "" });
 
     try {
-      const response = await fetch('http://localhost:3000/api/contact', {
+      const response = await fetch('https://connektixx.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
